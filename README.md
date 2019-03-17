1e7f9acebf8454ef04c2cefb72237d42084c9ac9
realdennis <realdennis777@gmail.com>
2019-03-17 21:49:59 +0800

    styles(font) & docs(initialText README)

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ https://md2pdf.netlify.com/
 ```
 
 ## How to use md2pdf?
-1. Click button choose `.md` file
+1. Click button choose `.md` file.
 2. Edit in editor (left panel).
 3. Click **Transform**!
+4. Switch 'Destination' to **Save as PDF**.
 4. **Chrome recommended**
 
 ## Tips
--- src/Container/Hooks/InitialText.js
+++ src/Container/Hooks/InitialText.js
@@ -1,6 +1,6 @@
 export const initialText = `
 # Markdown2PDF 
-
+https://md2pdf.netlify.com/
 > Awesome Markdown to PDF!
 \`\`\`diff
 - Online? Upload resume.md to stranger server?
@@ -8,9 +8,10 @@ export const initialText = `
 \`\`\`
 
 ## How to use md2pdf?
-1. Click button choose \`.md\` file
+1. Click button choose \`.md\` file.
 2. Edit in editor (left panel).
 3. Click **Transform**!
+4. Switch 'Destination' to **Save as PDF**.
 4. **Chrome recommended**
 
 ## Tips
