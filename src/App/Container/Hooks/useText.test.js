d3159b6b75ae67b329fe61a8bbf674621957e0a8
realdennis <realdennis777@gmail.com>
2019-03-21 04:37:22 +0800

    using react-testing-library instead of enzyme for testing lazy comp

--- src/App/Container/Hooks/useText.test.js
+++ src/App/Container/Hooks/useText.test.js
@@ -2,15 +2,13 @@ import { renderHook, cleanup, act } from 'react-hooks-testing-library';
 import useText from './useText';
 import { initialText } from './InitialText';
 
-describe('[Hooks] useText test', () => {
-  afterEach(cleanup);
-  it('should set initial', () => {
-    const { result } = renderHook(() => useText());
-    expect(result.current[0]).toEqual(initialText);
-  });
-  it('should change text', () => {
-    const { result } = renderHook(() => useText());
-    act(() => result.current[1]('work'));
-    expect(result.current[0]).toBe('work');
-  });
+afterEach(cleanup);
+test('should set initial', () => {
+  const { result } = renderHook(() => useText());
+  expect(result.current[0]).toEqual(initialText);
+});
+test('should change text', () => {
+  const { result } = renderHook(() => useText());
+  act(() => result.current[1]('work'));
+  expect(result.current[0]).toBe('work');
 });
