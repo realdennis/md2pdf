dcb61f89b9435dc1e909bc385e9647f691c35b98
ShizukuIchi <shizukuichi@gmail.com>
2019-03-22 17:04:17 +0800

    feat: support drop file

--- src/App/Components/Markdown/Editor/index.js
+++ src/App/Components/Markdown/Editor/index.js
@@ -3,8 +3,51 @@ import styled from 'styled-components';
 import EditArea from './EditArea.js';
 
 const Editor = ({ className, text, setText, width }) => {
+  const [uploading, setUploading] = React.useState(false);
+  function onDragLeave(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+  function onDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  }
+
+  function onDrop(e) {
+    handleUpload(e.dataTransfer.files);
+  }
+  function handleUpload(files) {
+    if (files && files[0] && !uploading) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        setUploading(false);
+        setText(e.target.result);
+      };
+      reader.readAsText(files[0]);
+      setUploading(true);
+    } else if (uploading) {
+      console.log('正在上傳，請稍等~');
+    }
+  }
+  React.useEffect(() => {
+    function preventDefault(e) {
+      e.preventDefault();
+    }
+    window.addEventListener('dragover', preventDefault);
+    window.addEventListener('drop', preventDefault);
+    return () => {
+      window.removeEventListener('dragover', preventDefault);
+      window.removeEventListener('drop', preventDefault);
+    };
+  });
   return (
-    <div style={{ width }} className={className}>
+    <div
+      style={{ width, opacity: uploading ? 0.5 : 1 }}
+      className={className}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
+    >
       <EditArea text={text} setText={setText}>
         {text}
       </EditArea>
