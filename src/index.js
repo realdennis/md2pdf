b48875c05b21334eb18186a00ce2c14d6d628c63
realdennis <realdennis777@gmail.com>
2019-03-23 19:45:12 +0800

    Prevent redirect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './styles.css';
-import App from './App'
+import App from './App';
 const rootElement = document.getElementById('root');
 
 // [Prevent] The redirect of file drop
-window.addEventListener('drop', e => e.preventDefault());
+window.addEventListener('drop', e => e.preventDefault(), true);
+// window.addEventListener('dragstart', e => e.preventDefault(), true);
+window.addEventListener('dragover', e => e.preventDefault(), true);
+// window.addEventListener('dragleave', e => e.preventDefault(), true);
 
 ReactDOM.render(<App />, rootElement);
