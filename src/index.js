5adc3fec682424b4d8f79bb553ee010d1841da21
realdennis <realdennis777@gmail.com>
2019-03-23 19:25:37 +0800

    Feat. redirect prevent

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import 'normalize.css';
 import './styles.css';
 import App from './App'
 const rootElement = document.getElementById('root');
+
+// [Prevent] The redirect of file drop
+window.addEventListener('drop', e => e.preventDefault());
+
 ReactDOM.render(<App />, rootElement);
