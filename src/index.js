d866b59e7c0436dfaf1e3cc2f7b18e3656baeb10
realdennis <realdennis777@gmail.com>
2019-11-21 15:56:32 +0000

    Add beforeunload event to prevent accident described on issue#30

--- src/index.js
+++ src/index.js
@@ -10,5 +10,9 @@ window.addEventListener('drop', e => e.preventDefault(), true);
 // window.addEventListener('dragstart', e => e.preventDefault(), true);
 window.addEventListener('dragover', e => e.preventDefault(), true);
 // window.addEventListener('dragleave', e => e.preventDefault(), true);
+window.addEventListener('beforeunload', e => {
+  const msg = 'Please check and backup the change before refresh or leave.';
+  return ((e || window.event).returnValue = msg);
+});
 
 ReactDOM.render(<App />, rootElement);
