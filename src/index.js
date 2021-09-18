5d18402cf29fd5e380dd529e01fb99a78121f3a2
realdennis <realdennis777@gmail.com>
2021-09-18 21:50:36 +0800

    feat(service-worker): integrate workbox to cra

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './styles.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const rootElement = document.getElementById('root');
 
 // [Prevent] The redirect of file drop
@@ -10,9 +11,10 @@ window.addEventListener('drop', e => e.preventDefault(), true);
 // window.addEventListener('dragstart', e => e.preventDefault(), true);
 window.addEventListener('dragover', e => e.preventDefault(), true);
 // window.addEventListener('dragleave', e => e.preventDefault(), true);
-window.addEventListener('beforeunload', e => {
+window.addEventListener('beforeunload', (e) => {
   const msg = 'Please check and backup the change before refresh or leave.';
   return ((e || window.event).returnValue = msg);
 });
 
 ReactDOM.render(<App />, rootElement);
+serviceWorkerRegistration.register();
